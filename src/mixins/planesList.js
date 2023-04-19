5d6b57790798be4975a2550aeddb61a5f5127162
augustobarbosa77 <augusto.carlos@omnik.com.br>
2023-04-18 21:39:16 -0300

    [002] criação da mixins de "planes"

---
+++ src/mixins/planesList.js
@@ -0,0 +1,106 @@
+const planes =
+[
+    {
+        id: '001',
+        name: 'Hospedagem 1',
+        price: 'Grátis',
+        monthlyPaymentDescription: 'você não paga nada para usar sem taxa de setup',
+        description: 'Ideal para quem está começando.',
+        serverDescription: 'Seu site em servidores no Estados Unidos.',
+        listOne: [
+            'Servidores em nossos data center americanos;',
+            'ASP,ASP.NET 2.0/3.0/3.5/ e 4.0/4.5/4.5.1/4.5.2 (medium trust) ou PHP 5.3, 5.4, 5.5, 5.6 e 7.0;',
+            '1 usuário de FTP para upload e download;',
+            '1 conta de e-mails profissionais;',
+            '1 subdomínio gratuito;',
+        ],
+        support: 'Suporte 24 horas, 7 dias por semana grátis;',
+        apps: 'Aplicativos disponíveis;',
+        listTwo: [
+            'Wordpress;',
+            'Drupal;',
+            'entre outros...'
+        ],
+        youHave: 'Você ainda tem',
+        listTree: [
+            'Webmail RoundCube;',
+            'AntiSpam;',
+            'Painel de gerenciamento de DNS;',
+            'Painel de controle web;',
+            'Configurações de PHP personalizáveis;',
+            'Certificado SSL Grátis;',
+            'Transferência ilimitada.'
+        ]
+    },
+    {
+        id: '002',
+        name: 'Hospedagem 2',
+        price: 'R$499/mês',
+        monthlyPaymentDescription: 'cobrado mensalmente sem taxa de setup',
+        description: 'Ideal para sites com mais de 30k de visitas.',
+        serverDescription: 'Seu site em servidores no Brasil.',
+        listOne: [
+            'Servidores em nosso data center de São Paulo;',
+            'ASP,ASP.NET 2.0/3.0/3.5/ e 4.0/4.5/4.5.1/4.5.2 (medium trust) ou PHP 5.3, 5.4, 5.5, 5.6 e 7.0;',
+            '10 usuários de FTP para upload e download;',
+            '30 contas de e-mails profissionais;',
+            '10 subdomínios gratuitos;',
+        ],
+        support: 'Suporte 24 horas, 7 dias por semana grátis;',
+        apps: 'Aplicativos disponíveis;',
+        listTwo: [
+            'Wordpress;',
+            'Drupal;',
+            'entre outros...'
+        ],
+        migrationFree: 'Migração Gratuita',
+        migrationFreeText: 'Migramos todos seus sites para nossos servidores;',
+        youHave: 'Você ainda tem',
+        listTree: [
+            'Webmail RoundCube;',
+            'AntiSpam;',
+            'Painel de gerenciamento de DNS;',
+            'Painel de controle web;',
+            'Configurações de PHP personalizáveis;',
+            'Certificado SSL Grátis;',
+            'Transferência ilimitada.'
+        ]
+    },
+    {
+        id: '003',
+        name: 'Hospedagem 3',
+        price: 'R$999/mês',
+        monthlyPaymentDescription: 'cobrado mensalmente sem taxa de setup',
+        description: 'Ideal para sites com mais de 60k de visitas.',
+        serverDescription: 'Seu site em servidores no Brasil.',
+        listOne: [
+            'Servidores em nosso data center de São Paulo;',
+            'ASP,ASP.NET 2.0/3.0/3.5/ e 4.0/4.5/4.5.1/4.5.2 (medium trust) ou PHP 5.3, 5.4, 5.5, 5.6 e 7.0;',
+            'ilimitados usuários de FTP para upload e download;',
+            'ilimitadas contas de e-mails profissionais;',
+            '50 subdomínios gratuitos;',
+        ],
+        support: 'Suporte 24 horas, 7 dias por semana grátis;',
+        apps: 'Aplicativos disponíveis;',
+        listTwo: [
+            'Wordpress;',
+            'Drupal;',
+            'entre outros...'
+        ],
+        migrationFree: 'Migração Gratuita',
+        migrationFreeText: 'Migramos todos seus sites para nossos servidores;',
+        youHave: 'Você ainda tem',
+        listTree: [
+            'Webmail RoundCube;',
+            'AntiSpam;',
+            'Painel de gerenciamento de DNS;',
+            'Painel de controle web;',
+            'Configurações de PHP personalizáveis;',
+            'Certificado SSL Grátis;',
+            'Transferência ilimitada.'
+        ]
+    },
+]
+
+export default planes
+  
